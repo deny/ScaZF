a6a959911c4112a645b3dca48a77be43769ee4e8
deny <danielkozka@gmail.com>
2012-04-08 14:43:52 +0200

    ~ dodane cache dla szablonów

--- tools/Model/Template.php
+++ tools/Model/Template.php
@@ -20,6 +20,20 @@ class Template
 	protected $aSubTemplates = array();
 
 	/**
+	 * Path to templates
+	 *
+	 * @var	string
+	 */
+	protected static $sTemplatePath = null;
+
+	/**
+	 * Loaded templates
+	 *
+	 * @var	array
+	 */
+	protected static $aTemplates = [];
+
+	/**
 	 * Constructor
 	 *
 	 * @param	string	$sFile	path to template file
@@ -34,13 +48,13 @@ class Template
 
 		$sTemplate = file_get_contents($sFile);
 
-
+		// template parsing
 		$iEnd = 0;
-		while(($iStart = strpos($sTemplate, '{@', $iEnd)) !== false)
+		while(($iStart = strpos($sTemplate, '{@begin', $iEnd)) !== false) // find section begin - start
 		{
-			$iTmp = strpos($sTemplate, '@}', $iStart);
-			$sTag = substr($sTemplate, $iStart + 8, $iTmp - $iStart - 8);
-			$iEnd = strpos($sTemplate, '{@end='. $sTag .'@}', $iStart);
+			$iTmp = strpos($sTemplate, '@}', $iStart); // find section begin - end
+			$sTag = substr($sTemplate, $iStart + 8, $iTmp - $iStart - 8); // get section name
+			$iEnd = strpos($sTemplate, '{@end='. $sTag .'@}', $iStart); // find section end
 
 			$this->aSubTemplates[$sTag] =  ltrim(substr($sTemplate, $iTmp + 2, $iEnd - $iTmp - 2), "\n");
 
@@ -48,6 +62,13 @@ class Template
 		}
 	}
 
+	/**
+	 * Return subtemplate filled with values
+	 *
+	 * @param	string	$sName		template name
+	 * @param	array	$aParams	params for template
+	 * @return	string
+	 */
 	public function getSubTemplate($sName, array $aParams = [])
 	{
 		if(!isset($this->aSubTemplates[$sName]))
@@ -63,5 +84,36 @@ class Template
 		}
 
 		return $sTmp;
+	}
+
+// STATIC
+
+	/**
+	 * Set templates path
+	 *
+	 * @param	string	$sPath	tempaltes path
+	 * @return	void
+	 */
+	public static function setTemplatesPath($sPath)
+	{
+		self::$sTemplatePath = $sPath;
+	}
+
+	/**
+	 * Return template
+	 *
+	 * @param	string	$sName	template name
+	 * @return	\ScaZF\Tool\Model\Template
+	 */
+	public static function getTemplate($sName)
+	{
+		if(!isset(self::$aTemplates[$sName]))
+		{
+			self::$aTemplates[$sName] = new self(
+				self::$sTemplatePath . '/'. $sName . '.tpl'
+			);
+		}
+
+		return self::$aTemplates[$sName];
 	}
 }
