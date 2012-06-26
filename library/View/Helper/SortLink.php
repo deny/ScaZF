7572eb526e98b88d2b2333aa324fe8a6ef16b8a0
daniel <daniel@deny.(none)>
2012-06-26 18:50:35 +0200

    + view helper do wyświetlania linków sortujących w CRUD

---
+++ library/View/Helper/SortLink.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Return sort links for crud
+ *
+ * @author	Daniel Kózka
+ */
+class Sca_View_Helper_SortLink extends Zend_View_Helper_Abstract
+{
+	/**
+	 * Helepr function
+	 *
+	 * @return	string
+	 */
+	public function sortLink($sName, $sAction)
+	{
+		$aUsedSort = isset($this->view->aUsedSort) ? $this->view->aUsedSort : array(0,0);
+		$bCurrent = $sName == $aUsedSort[0];
+		$bAsc = $bCurrent && $aUsedSort[1] == 'asc';
+		$sType = $bAsc ? 'desc' : 'asc';
+
+		$sResult = '<a '.
+						'class="sca-sl '.
+							($bAsc ? 'asc' : 'desc').
+							($bCurrent ? ' sca-sl-current' : '') .
+						'" href="'.
+							$sAction . $this->view->sPageUrl .'/sort/'. $sName .':'. $sType .
+					'">'.
+						$sName .
+					'</a>';
+
+		return $sResult;
+	}
+}
